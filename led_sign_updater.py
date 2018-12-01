0f60d39a83f708e87a9281b5d9051025b8ef7eb0
tazard <tazard@gmail.com>
2018-12-01 09:04:46 +0800

    Initial upload of working code from home repo

---
+++ led_sign_updater.py
@@ -0,0 +1,325 @@
+"""
+Script to poll for new tweet files and update LED sign as required
+
+Python 2.7 only
+"""
+
+from __future__ import print_function
+import glob
+import random
+import json
+from datetime import datetime
+import sys
+import time
+import ledsign2
+from tweet import Tweet
+
+transitions_to_title = [
+    ledsign2.EFFECT_SPLIT_OPEN,
+    ledsign2.EFFECT_WIPE_DOWN,
+    ledsign2.EFFECT_FALLING_LINES,
+]
+
+transitions_to_tweet = [
+    ledsign2.EFFECT_SPLIT_OPEN,
+    ledsign2.EFFECT_SPLIT_CLOSE,
+    ledsign2.EFFECT_WIPE_OUT,
+    ledsign2.EFFECT_WIPE_RIGHT,
+    ledsign2.EFFECT_WIPE_IN,
+    ledsign2.EFFECT_SPLIT_INTERLACED,
+    ledsign2.EFFECT_WIPE_INTERLACED,
+    ledsign2.EFFECT_WIPE_UP,
+    ledsign2.EFFECT_EXPLODE,
+    ledsign2.EFFECT_PACMAN,
+    ledsign2.EFFECT_PACMAN,
+    ledsign2.EFFECT_PACMAN,
+    ledsign2.EFFECT_SHOOT,
+    ledsign2.EFFECT_DISSOLVE,
+    ledsign2.EFFECT_SLIDE_LETTERS,
+]
+
+
+class TweetStack(object):
+    screen_width = 13
+
+    def __init__(self, size, persistence_path, sign_port, twitter_address, sign_scroll_wait):
+        self.size = size
+        self.persistence_path = persistence_path
+        self.sign_port = sign_port
+        self.twitter_address = twitter_address
+        self.sign_scroll_wait = sign_scroll_wait
+
+        self.tweets = []
+        self.has_new = False
+
+        self.check_for_update()
+
+    def check_for_update(self):
+        # List all tweets in directory in filename order (date time order)
+        file_paths = sorted(glob.glob(self.persistence_path.format('*')))
+
+        # We are interested in only the last "self.size" tweets
+        file_paths = file_paths[-self.size:]
+
+        # Check if there are more or fewer tweets that should be displayed
+        different = not (len(self.tweets) == len(file_paths))
+
+        if not different:
+            # Check through the paths
+            for (tweet_path, _), file_path in zip(self.tweets, file_paths):
+                if tweet_path != file_path:
+                    different = True
+                    break
+
+        if different:
+            # Reload the tweets since there is a difference
+            print("Found difference in tweets on disk")
+            self.load_from_disk(file_paths)
+
+    def load_from_disk(self, file_paths):
+        # Dump existing tweets
+        self.tweets = []
+
+        # Load last 5 tweets (or less if fewer)
+        for path in file_paths[-5:]:
+            # If there is a problem loading tweet file, skip over it
+            try:
+                tweet = Tweet.from_file(path)
+            except ValueError as e:
+                print("Cannot decode tweet file '{}': {}".format(path, e))
+            finally:
+                self.tweets.append([path, tweet])
+
+                # Flag that at least one tweet is new
+                # (only flag when actually loading tweet)
+                self.has_new = True
+
+    # def add_tweet(self, tweet):
+    #     tweet.save_to_file(self.persistence_path)
+    #     self.tweets.append(tweet)
+    #     if len(self.tweets) > self.size:
+    #         self.tweets.pop(0)
+
+    def split_text(self, text):
+        length = len(text)
+
+        # Want to round up
+        panels = float(length) / float(self.screen_width)
+        if int(panels) != panels:
+            panels += 1
+        panels = int(panels)
+
+        for idx in range(panels):
+            yield "{:<13}".format(text[idx * self.screen_width: (idx + 1) * self.screen_width])
+
+    def update_sign(self):
+        sign = ledsign2.LEDSign(self.sign_port)
+        sign.begin_message(reset=True)
+        sign.begin_file(1)
+
+        # Newest tweet first
+        tweets_to_show = self.tweets[::-1]
+
+        if self.has_new:
+            self.has_new = False
+            # Show newest tweet with special "just in" headline first
+            if len(tweets_to_show) > 0:
+                # Pop it off so not included below only when this runs
+                _, tweet = tweets_to_show.pop(0)
+                self.draw_tweet_just_in(sign)
+                self.draw_username(sign, tweet)
+                self.draw_message(sign, tweet)
+                sign.end_frame()
+
+        # Now show title
+        self.draw_title(sign)
+
+        # Promote twitter address for this sign
+        self.draw_call_for_tweets(sign)
+
+        # Back to the rest of the tweets
+        for _, tweet in tweets_to_show:
+            self.draw_tweet_headline(sign)
+            self.draw_username(sign, tweet)
+            self.draw_message(sign, tweet)
+
+        # if len(self.tweets) == 0:
+        #     # No tweets, encourage some
+        #     self.draw_call_for_tweets(sign)
+
+        sign.end_file()
+        sign.end_message()
+
+    def draw_title(self, sign):
+        sign.add_run_mode(ledsign2.EFFECT_SCROLL_UP)
+        sign.add_special(ledsign2.FONT_5x5)
+        sign.add_special(ledsign2.COLOUR_RAINBOW2)
+        sign.add_text("Artifactory")
+        sign.end_frame()
+        sign.add_run_mode(random.choice(transitions_to_title))
+        sign.add_special(ledsign2.FONT_5x5)
+        sign.add_special(ledsign2.COLOUR_RAINBOW1)
+        sign.add_text("Twittinator")
+
+    def draw_tweet_headline(self, sign):
+        sign.end_frame()
+
+        sign.add_run_mode(ledsign2.EFFECT_SCROLL_UP)
+        sign.add_special(ledsign2.FONT_5x7)
+
+        sign.add_special(ledsign2.COLOUR_RAINBOW4)
+        sign.add_special(ledsign2.SYMBOL_STARBURST)
+
+        sign.add_special(ledsign2.COLOUR_BRIGHT_ORANGE)
+        sign.add_text("Tweet")
+
+        sign.add_special(ledsign2.COLOUR_RAINBOW4)
+        sign.add_special(ledsign2.SYMBOL_STARBURST)
+
+    def draw_call_for_tweets(self, sign):
+        sign.end_frame()
+
+        sign.add_run_mode(ledsign2.EFFECT_SCROLL_UP)
+        sign.add_special(ledsign2.FONT_5x7)
+        sign.add_special(ledsign2.COLOUR_BRIGHT_ORANGE)
+        sign.add_text(" Tweet to: ")
+
+        text = "@{}".format(self.twitter_address)
+        if self.sign_scroll_wait:
+            for bit in self.split_text(text):
+                sign.end_frame()
+                sign.add_run_mode(ledsign2.EFFECT_SCROLL_LEFT)
+                sign.add_special(ledsign2.FONT_5x7)
+                sign.add_special(ledsign2.COLOUR_BRIGHT_GREEN)
+                sign.add_text(bit)
+        else:
+            sign.end_frame()
+            sign.add_run_mode(ledsign2.EFFECT_SCROLL_LEFT)
+            sign.add_special(ledsign2.FONT_5x7)
+            sign.add_special(ledsign2.COLOUR_BRIGHT_GREEN)
+            sign.add_text(text)
+
+    def draw_tweet_just_in(self, sign):
+        sign.add_run_mode(ledsign2.EFFECT_SCROLL_UP)
+        sign.add_special(ledsign2.FONT_5x7)
+
+        sign.add_special(ledsign2.COLOUR_RAINBOW3)
+        sign.add_special(ledsign2.SYMBOL_STARBURST)
+
+        sign.add_special(ledsign2.COLOUR_YELLOW)
+        sign.add_text("Just In")
+
+        sign.add_special(ledsign2.COLOUR_RAINBOW3)
+        sign.add_special(ledsign2.SYMBOL_STARBURST)
+
+    def draw_username(self, sign, tweet):
+        text = "@" + tweet.screen_name + ": "
+        # text = "@" + tweet.user + ": "
+        first = True
+        for bit in self.split_text(text):
+            sign.end_frame()
+
+            if first:
+                sign.add_run_mode(random.choice(transitions_to_tweet))
+                first = False
+            else:
+                sign.add_run_mode(ledsign2.EFFECT_SCROLL_LEFT)
+
+            sign.add_special(ledsign2.COLOUR_BRIGHT_GREEN)
+            sign.add_special(ledsign2.FONT_5x7)
+            sign.add_text(bit)
+
+    def draw_message(self, sign, tweet):
+        text = tweet.text
+
+        if self.sign_scroll_wait:
+            # Scroll one 13 character frame at a time
+            for bit in self.split_text(text):
+                sign.end_frame()
+
+                sign.add_run_mode(ledsign2.EFFECT_SCROLL_LEFT)
+                sign.add_special(ledsign2.COLOUR_BRIGHT_RED)
+                sign.add_special(ledsign2.FONT_5x7)
+                sign.add_text(bit)
+        else:
+            # Scroll the whole thing at once
+            sign.end_frame()
+            sign.add_run_mode(ledsign2.EFFECT_SCROLL_LEFT)
+            sign.add_special(ledsign2.COLOUR_BRIGHT_RED)
+            sign.add_special(ledsign2.FONT_5x7)
+            sign.add_text(text)
+
+
+class LEDSignUpdater(object):
+    def __init__(self, config_path):
+        print("Loading config")
+
+        with open(config_path) as f:
+            j_config = json.load(f)
+
+        self.stack_size = j_config["stack_size"]
+        self.tweet_path = j_config["tweet_path"]
+        self.sign_port = j_config["sign_port"]
+        self.twitter_address = j_config["twitter_address"]
+        self.sign_scroll_wait = j_config["sign_scroll_wait"]
+        self.update_time_min_sec = j_config["update_time_min_sec"]
+        self.update_time_force_sec = j_config["update_time_force_sec"]
+
+        print("Loading latest {} cached tweets".format(self.stack_size))
+        self.tweet_stack = TweetStack(
+            size=self.stack_size,
+            persistence_path=self.tweet_path,
+            sign_port=self.sign_port,
+            twitter_address=self.twitter_address,
+            sign_scroll_wait=self.sign_scroll_wait,
+        )
+        print("{} tweets loaded".format(len(self.tweet_stack.tweets)))
+
+        self.last_update = None
+
+        self.update()
+
+    def update(self):
+        print("Updating sign")
+        self.tweet_stack.update_sign()
+        self.last_update = datetime.utcnow()
+
+    def run(self):
+        # Check for new files
+        try:
+            self.tweet_stack.check_for_update()
+        except IOError as e:
+            print("Error opening file, will try again later: {}".format(e))
+            return
+
+        now = datetime.utcnow()
+
+        # If new, update sign (as long as not too recently updated)
+        if self.tweet_stack.has_new and (now - self.last_update).total_seconds() > self.update_time_min_sec:
+            # We have a new tweet and its not too soon since we last updated
+            print("Displaying new tweet that arrived")
+            self.update()
+
+        # Update sign every 5 minutes (clears "new" message and cycles animations)
+        if (now - self.last_update).total_seconds() > self.update_time_force_sec:
+            print("Periodic sign update")
+            self.update()
+
+
+def main(args):
+    print("Artifactory Twittinator - LED Sign Updater")
+
+    if len(args) != 1:
+        print("Requires exactly one argument as path to config")
+        sys.exit(1)
+
+    updater = LEDSignUpdater(config_path=args[0])
+
+    # Main processing loop
+    while True:
+        updater.run()
+        time.sleep(1.0)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ ledsign2.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python
+"""
+Driver for the Bestlink Optoelectronics M500N-7X50R1 / M500N-7X80RG2
+Copyright 2010 Michael Farrell <http://micolous.id.au/>
+
+This sign is also sometimes known as XC-0191, XC-0193 or XC-0198 (as sold by
+Jaycar and Procon Technology in AU).
+
+Requires pyserial library in order to interface.
+
+This library is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+import serial, string
+from datetime import datetime, time
+from struct import pack
+
+# Both open and close effects
+# cycle through all effects
+EFFECT_CYCLIC = 1
+EFFECT_IMMEDIATE = 2
+EFFECT_SCROLL_LEFT = 3
+EFFECT_SCROLL_RIGHT = 4
+EFFECT_SPLIT_OPEN = 5
+EFFECT_SPLIT_CLOSE = 6
+EFFECT_WIPE_OUT = 7
+EFFECT_WIPE_LEFT = 8
+EFFECT_WIPE_RIGHT = 9
+EFFECT_WIPE_IN = 10
+EFFECT_SCROLL_UP = 11
+EFFECT_SCROLL_DOWN = 12
+EFFECT_SPLIT_INTERLACED = 13
+EFFECT_WIPE_INTERLACED = 14
+EFFECT_WIPE_UP = 15
+EFFECT_WIPE_DOWN = 16
+EFFECT_WIPE_LINE = 17
+EFFECT_EXPLODE = 18
+EFFECT_PACMAN = 19
+EFFECT_FALLING_LINES = 20
+EFFECT_SHOOT = 21
+EFFECT_BLINK = 22
+EFFECT_DISSOLVE = 23
+EFFECT_SLIDE_LETTERS = 24
+
+SYMBOL_STARBURST = '\x60'
+SYMBOL_SNAKE = '\x61'
+SYMBOL_UMBRELLA = '\x62'
+SYMBOL_CLOCK = '\x63'
+SYMBOL_TELEPHONE = '\x64'
+SYMBOL_GLASSES = '\x65'
+SYMBOL_TAP = '\x66'
+SYMBOL_ROCKET = '\x67'
+SYMBOL_CRAB = '\x68'
+SYMBOL_KEY = '\x69'
+SYMBOL_SHIRT = '\x6A'
+SYMBOL_HELECOPTER = '\x6B'
+SYMBOL_CAR = '\x6C'
+SYMBOL_TANK = '\x6D'
+SYMBOL_HOUSE = '\x6E'
+SYMBOL_LANTERN = '\x6F'
+SYMBOL_TREES = '\x70'
+SYMBOL_DUCK = '\x71'
+SYMBOL_SCOOTER = '\x72'
+SYMBOL_BIKE = '\x73'
+SYMBOL_CROWN = '\x74'
+SYMBOL_BUTTERFLY = '\x75'
+SYMBOL_RIGHT = '\x76'
+SYMBOL_LEFT = '\x77'
+SYMBOL_DOWN_LEFT = '\x78'
+SYMBOL_UP_LEFT = '\x79'
+SYMBOL_CUP = '\x7A'
+SYMBOL_CHAIR = '\x7B'
+SYMBOL_SHOE = '\x7C'
+SYMBOL_MARTINI = '\x7D'
+
+SPECIAL_TIME = '\x80'
+SPECIAL_DATE = '\x81'
+
+# requires the larger version of the sign to display properly
+# still exists in the small version.
+ANIMATION_MERRY_XMAS = '\x90'
+ANIMATION_HAPPY_NEW_YEAR = '\x91'
+ANIMATION_4TH_JULY = '\x92'
+ANIMATION_HAPPY_EASTER = '\x93'
+ANIMATION_HAPPY_HALLOWEEN = '\x94'
+ANIMATION_DONT_DRINK_DRIVE = '\x95'
+ANIMATION_NO_SMOKING = '\x96'
+ANIMATION_WELCOME = '\x97'
+# extra undocumented copy of the graphics here, \x98 - \x9F
+
+FONT_5x5 = '\xA0'
+FONT_10x5 = '\xA1'
+FONT_5x7 = '\xA2'
+FONT_10x7 = '\xA3'
+FONT_8x7 = '\xA4'
+FONT_16x7 = '\xA5'
+FONT_SMALL_FONTS = '\xA6'
+# broken extra fonts
+FONT_3x7 = '\xAC'
+
+COLOUR_BRIGHT_RED = '\xB0'
+COLOUR_DIM_RED = '\xB1'
+# additional colours for M500N-7X80RG2 (Red/Green 2-LED version)
+# thanks to Martin Hill <martin at eshock.com>
+COLOUR_AMBER = '\xB2'
+COLOUR_YELLOW = '\xB3'
+COLOUR_DIM_ORANGE = '\xB4'
+COLOUR_BRIGHT_ORANGE = '\xB5'
+COLOUR_DIM_GREEN = '\xB6'
+COLOUR_BRIGHT_GREEN = '\xB7'
+COLOUR_RAINBOW1 = '\xB8'
+COLOUR_RAINBOW2 = '\xB9'
+COLOUR_RAINBOW3 = '\xBA'
+COLOUR_RAINBOW4 = '\xBB'
+COLOUR_RAINBOW5 = '\xBC'
+COLOUR_RAINBOW6 = '\xBD'
+COLOUR_RAINBOW7 = '\xBE'
+COLOUR_RAINBOW8 = '\xBF'
+
+# fastest speed is 1, slowest is 8
+SPEED_1 = '\xC0'
+SPEED_2 = '\xC1'
+SPEED_3 = '\xC2'
+SPEED_4 = '\xC3'
+SPEED_5 = '\xC4'
+SPEED_6 = '\xC5'
+SPEED_7 = '\xC6'
+SPEED_8 = '\xC7'
+
+# fastest pause time is 1, slowest is 8
+PAUSE_1 = '\xC8'
+PAUSE_2 = '\xC9'
+PAUSE_3 = '\xCA'
+PAUSE_4 = '\xCB'
+PAUSE_5 = '\xCC'
+PAUSE_6 = '\xCD'
+PAUSE_7 = '\xCE'
+PAUSE_8 = '\xCF'
+
+# TODO: implement programming custom graphics
+GRAPHIC_CUSTOM_1 = '\xD0'
+GRAPHIC_CUSTOM_2 = '\xD1'
+GRAPHIC_CUSTOM_3 = '\xD2'
+GRAPHIC_CUSTOM_4 = '\xD3'
+GRAPHIC_CUSTOM_5 = '\xD4'
+GRAPHIC_CUSTOM_6 = '\xD5'
+GRAPHIC_CUSTOM_7 = '\xD6'
+GRAPHIC_CUSTOM_8 = '\xD7'
+# Multiple interpretations of the graphics here.
+# Trains, teacups, beach and mouse is what I thought they were.
+# I've updated it so it has the "official names" of these graphics from the
+# manual as well, but not break the API.
+GRAPHIC_CITY = GRAPHIC_TRAINS = '\xD8'
+GRAPHIC_CARS = '\xD9'
+GRAPHIC_VESSEL = GRAPHIC_TEACUPS = '\xDA'
+GRAPHIC_TELEPHONE = '\xDB'
+GRAPHIC_NATURE = GRAPHIC_BEACH = '\xDC'
+GRAPHIC_SHIP = '\xDD'
+GRAPHIC_SWIM = '\xDE'
+GRAPHIC_CAT = GRAPHIC_MOUSE = '\xDF'
+
+SOUND_BEEP_3 = '\xE0'
+SOUND_BEEP_5 = '\xE1'
+SOUND_BEEP_1 = '\xE2'
+
+
+class LEDSign:
+    """Implementation of the XC0193 LED Matrix Sign protocol"""
+
+    def __init__(self, port):
+        self.s = serial.Serial(port, 2400)
+        self.file_id = None
+        self.message_open = False
+        # print("Opening Port %s" % self.s.portstr)
+
+    def send_to_sign(self, msg):
+        # byte_string = " ".join("{:02x}".format(ord(c)) for c in msg)
+        # print('Send: "{}" = "{}"'.format(msg, byte_string))
+        self.s.write(msg)
+
+    def begin_message(self, sign=range(0, 128), reset=False):
+        """Begins a message for a sign.  defaults to all signs."""
+        if self.message_open:
+            raise Exception("A message is already open")
+
+        try:
+            if len(sign) == 1:
+                sign = sign[0]
+        except TypeError:
+            pass
+
+        if type(sign) == int or type(sign) == long:
+            # we're only programming a single sign.
+            # we can optimise!
+            if sign < 0 or sign > 127:
+                raise Exception("cannot send to sign ID outside of range 0-127, you sent to sign %s" % sign)
+
+            # TODO: check whether this should be little or big endian properly.
+            # the manual isn't really clear on this.  so lets take a stab in the dark.
+
+            self.send_to_sign("\x00" + chr(sign) + chr(sign))
+
+            if reset:
+                self.send_to_sign("\x01")
+            else:
+                self.send_to_sign("\x00")
+
+        # we're done...
+
+        else:
+            # this is a list with multiple members:
+            for x in sign:
+                if x < 0 or x > 127:
+                    raise Exception("cannot send to sign ID outside of range 0-127, you sent to sign %s" % x)
+
+            # this is a special thing, doesn't have the \xFF terminator
+            # start programming the sign, do not reset memory
+            self.send_to_sign("\x00\xFF\xFF")
+            if reset:
+                self.send_to_sign("\x01")
+            else:
+                self.send_to_sign("\x00")
+
+            # # data is for sign list
+            # self.send_to_sign("\x0B")
+            #
+            # if sign == range(0, 128):
+            #     # we're sending data to everyone.
+            #     # TODO: test that this actually works
+            #     self.send_to_sign('\xFF')
+            # else:
+            #     for x in sign:
+            #         self.send_to_sign(chr(x))
+            # self.send_to_sign('\xFF')
+
+        self.message_open = True
+
+    def add_run_mode(self, mode=1):
+        if mode < 0 or mode > 24:
+            raise Exception("run mode must be 0-24")
+
+        self.send_to_sign(chr(mode))
+
+    def add_text(self, msg):
+        msg = str(msg)
+        for x in msg:
+            y = ord(x)
+            if y < 32 or y > 237:
+                raise Exception("You shouldn't be using the character %s (#%s)." % (x, ord(x)))
+        self.send_to_sign(msg)
+
+    def add_special(self, special):
+        self.send_to_sign("\xEF" + special)
+
+    def end_frame(self):
+        self.send_to_sign('\xff')
+
+    def end_message(self):
+        if not self.message_open:
+            raise Exception("A message isn't open")
+        # this is a special thing, doesn't have the \xFF terminator
+        self.send_to_sign("\x00")
+        self.message_open = False
+
+    def set_clock(self, n=None, hour24=True):
+        if self.file_id is not None:
+            raise Exception("Cannot set clock while a file is open")
+        if n is None:
+            n = datetime.now()
+        if hour24:
+            hour24 = "\x00"
+        else:
+            hour24 = "\x01"
+        args = (pack("!B", n.isoweekday() % 7), hour24, n.year % 100, n.month, n.day, n.hour, n.minute, n.second)
+        self.send_to_sign(("\x08%s%s" + ("%02d" * 6) + '\xFF') % args)
+
+    def begin_file(self, file_id):
+        if self.file_id is not None:
+            raise Exception("A file, %s, is already open" % self.file_id)
+        file_id = int(file_id)
+        if file_id < 0 or file_id > 99:
+            raise Exception("file_id must be between 0 and 99.")
+
+        self.send_to_sign("\x01%02d" % file_id)
+        self.file_id = file_id
+
+    def end_file(self):
+        if self.file_id is None:
+            raise Exception("No file is open")
+        self.send_to_sign("\xFF\xFF")
+        self.file_id = None
+
+    def display_page(self, pageid):
+        pass
+
+    def playlist(self, page_order):
+        pass
+
+    def close(self):
+        self.s.close()
