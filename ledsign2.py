1764bbfdd89bcac93059b7b9e82b0290f940f075
Blake Samuels <16077190+tazard@users.noreply.github.com>
2020-06-14 15:28:03 +0800

    Python 3 update

--- ledsign2.py
+++ ledsign2.py
@@ -52,126 +52,126 @@ EFFECT_BLINK = 22
 EFFECT_DISSOLVE = 23
 EFFECT_SLIDE_LETTERS = 24
 
-SYMBOL_STARBURST = '\x60'
-SYMBOL_SNAKE = '\x61'
-SYMBOL_UMBRELLA = '\x62'
-SYMBOL_CLOCK = '\x63'
-SYMBOL_TELEPHONE = '\x64'
-SYMBOL_GLASSES = '\x65'
-SYMBOL_TAP = '\x66'
-SYMBOL_ROCKET = '\x67'
-SYMBOL_CRAB = '\x68'
-SYMBOL_KEY = '\x69'
-SYMBOL_SHIRT = '\x6A'
-SYMBOL_HELECOPTER = '\x6B'
-SYMBOL_CAR = '\x6C'
-SYMBOL_TANK = '\x6D'
-SYMBOL_HOUSE = '\x6E'
-SYMBOL_LANTERN = '\x6F'
-SYMBOL_TREES = '\x70'
-SYMBOL_DUCK = '\x71'
-SYMBOL_SCOOTER = '\x72'
-SYMBOL_BIKE = '\x73'
-SYMBOL_CROWN = '\x74'
-SYMBOL_BUTTERFLY = '\x75'
-SYMBOL_RIGHT = '\x76'
-SYMBOL_LEFT = '\x77'
-SYMBOL_DOWN_LEFT = '\x78'
-SYMBOL_UP_LEFT = '\x79'
-SYMBOL_CUP = '\x7A'
-SYMBOL_CHAIR = '\x7B'
-SYMBOL_SHOE = '\x7C'
-SYMBOL_MARTINI = '\x7D'
+SYMBOL_STARBURST = b'\x60'
+SYMBOL_SNAKE = b'\x61'
+SYMBOL_UMBRELLA = b'\x62'
+SYMBOL_CLOCK = b'\x63'
+SYMBOL_TELEPHONE = b'\x64'
+SYMBOL_GLASSES = b'\x65'
+SYMBOL_TAP = b'\x66'
+SYMBOL_ROCKET = b'\x67'
+SYMBOL_CRAB = b'\x68'
+SYMBOL_KEY = b'\x69'
+SYMBOL_SHIRT = b'\x6A'
+SYMBOL_HELECOPTER = b'\x6B'
+SYMBOL_CAR = b'\x6C'
+SYMBOL_TANK = b'\x6D'
+SYMBOL_HOUSE = b'\x6E'
+SYMBOL_LANTERN = b'\x6F'
+SYMBOL_TREES = b'\x70'
+SYMBOL_DUCK = b'\x71'
+SYMBOL_SCOOTER = b'\x72'
+SYMBOL_BIKE = b'\x73'
+SYMBOL_CROWN = b'\x74'
+SYMBOL_BUTTERFLY = b'\x75'
+SYMBOL_RIGHT = b'\x76'
+SYMBOL_LEFT = b'\x77'
+SYMBOL_DOWN_LEFT = b'\x78'
+SYMBOL_UP_LEFT = b'\x79'
+SYMBOL_CUP = b'\x7A'
+SYMBOL_CHAIR = b'\x7B'
+SYMBOL_SHOE = b'\x7C'
+SYMBOL_MARTINI = b'\x7D'
 
-SPECIAL_TIME = '\x80'
-SPECIAL_DATE = '\x81'
+SPECIAL_TIME = b'\x80'
+SPECIAL_DATE = b'\x81'
 
 # requires the larger version of the sign to display properly
 # still exists in the small version.
-ANIMATION_MERRY_XMAS = '\x90'
-ANIMATION_HAPPY_NEW_YEAR = '\x91'
-ANIMATION_4TH_JULY = '\x92'
-ANIMATION_HAPPY_EASTER = '\x93'
-ANIMATION_HAPPY_HALLOWEEN = '\x94'
-ANIMATION_DONT_DRINK_DRIVE = '\x95'
-ANIMATION_NO_SMOKING = '\x96'
-ANIMATION_WELCOME = '\x97'
+ANIMATION_MERRY_XMAS = b'\x90'
+ANIMATION_HAPPY_NEW_YEAR = b'\x91'
+ANIMATION_4TH_JULY = b'\x92'
+ANIMATION_HAPPY_EASTER = b'\x93'
+ANIMATION_HAPPY_HALLOWEEN = b'\x94'
+ANIMATION_DONT_DRINK_DRIVE = b'\x95'
+ANIMATION_NO_SMOKING = b'\x96'
+ANIMATION_WELCOME = b'\x97'
 # extra undocumented copy of the graphics here, \x98 - \x9F
 
-FONT_5x5 = '\xA0'
-FONT_10x5 = '\xA1'
-FONT_5x7 = '\xA2'
-FONT_10x7 = '\xA3'
-FONT_8x7 = '\xA4'
-FONT_16x7 = '\xA5'
-FONT_SMALL_FONTS = '\xA6'
+FONT_5x5 = b'\xA0'
+FONT_10x5 = b'\xA1'
+FONT_5x7 = b'\xA2'
+FONT_10x7 = b'\xA3'
+FONT_8x7 = b'\xA4'
+FONT_16x7 = b'\xA5'
+FONT_SMALL_FONTS = b'\xA6'
 # broken extra fonts
-FONT_3x7 = '\xAC'
+FONT_3x7 = b'\xAC'
 
-COLOUR_BRIGHT_RED = '\xB0'
-COLOUR_DIM_RED = '\xB1'
+COLOUR_BRIGHT_RED = b'\xB0'
+COLOUR_DIM_RED = b'\xB1'
 # additional colours for M500N-7X80RG2 (Red/Green 2-LED version)
 # thanks to Martin Hill <martin at eshock.com>
-COLOUR_AMBER = '\xB2'
-COLOUR_YELLOW = '\xB3'
-COLOUR_DIM_ORANGE = '\xB4'
-COLOUR_BRIGHT_ORANGE = '\xB5'
-COLOUR_DIM_GREEN = '\xB6'
-COLOUR_BRIGHT_GREEN = '\xB7'
-COLOUR_RAINBOW1 = '\xB8'
-COLOUR_RAINBOW2 = '\xB9'
-COLOUR_RAINBOW3 = '\xBA'
-COLOUR_RAINBOW4 = '\xBB'
-COLOUR_RAINBOW5 = '\xBC'
-COLOUR_RAINBOW6 = '\xBD'
-COLOUR_RAINBOW7 = '\xBE'
-COLOUR_RAINBOW8 = '\xBF'
+COLOUR_AMBER = b'\xB2'
+COLOUR_YELLOW = b'\xB3'
+COLOUR_DIM_ORANGE = b'\xB4'
+COLOUR_BRIGHT_ORANGE = b'\xB5'
+COLOUR_DIM_GREEN = b'\xB6'
+COLOUR_BRIGHT_GREEN = b'\xB7'
+COLOUR_RAINBOW1 = b'\xB8'
+COLOUR_RAINBOW2 = b'\xB9'
+COLOUR_RAINBOW3 = b'\xBA'
+COLOUR_RAINBOW4 = b'\xBB'
+COLOUR_RAINBOW5 = b'\xBC'
+COLOUR_RAINBOW6 = b'\xBD'
+COLOUR_RAINBOW7 = b'\xBE'
+COLOUR_RAINBOW8 = b'\xBF'
 
 # fastest speed is 1, slowest is 8
-SPEED_1 = '\xC0'
-SPEED_2 = '\xC1'
-SPEED_3 = '\xC2'
-SPEED_4 = '\xC3'
-SPEED_5 = '\xC4'
-SPEED_6 = '\xC5'
-SPEED_7 = '\xC6'
-SPEED_8 = '\xC7'
+SPEED_1 = b'\xC0'
+SPEED_2 = b'\xC1'
+SPEED_3 = b'\xC2'
+SPEED_4 = b'\xC3'
+SPEED_5 = b'\xC4'
+SPEED_6 = b'\xC5'
+SPEED_7 = b'\xC6'
+SPEED_8 = b'\xC7'
 
 # fastest pause time is 1, slowest is 8
-PAUSE_1 = '\xC8'
-PAUSE_2 = '\xC9'
-PAUSE_3 = '\xCA'
-PAUSE_4 = '\xCB'
-PAUSE_5 = '\xCC'
-PAUSE_6 = '\xCD'
-PAUSE_7 = '\xCE'
-PAUSE_8 = '\xCF'
+PAUSE_1 = b'\xC8'
+PAUSE_2 = b'\xC9'
+PAUSE_3 = b'\xCA'
+PAUSE_4 = b'\xCB'
+PAUSE_5 = b'\xCC'
+PAUSE_6 = b'\xCD'
+PAUSE_7 = b'\xCE'
+PAUSE_8 = b'\xCF'
 
 # TODO: implement programming custom graphics
-GRAPHIC_CUSTOM_1 = '\xD0'
-GRAPHIC_CUSTOM_2 = '\xD1'
-GRAPHIC_CUSTOM_3 = '\xD2'
-GRAPHIC_CUSTOM_4 = '\xD3'
-GRAPHIC_CUSTOM_5 = '\xD4'
-GRAPHIC_CUSTOM_6 = '\xD5'
-GRAPHIC_CUSTOM_7 = '\xD6'
-GRAPHIC_CUSTOM_8 = '\xD7'
+GRAPHIC_CUSTOM_1 = b'\xD0'
+GRAPHIC_CUSTOM_2 = b'\xD1'
+GRAPHIC_CUSTOM_3 = b'\xD2'
+GRAPHIC_CUSTOM_4 = b'\xD3'
+GRAPHIC_CUSTOM_5 = b'\xD4'
+GRAPHIC_CUSTOM_6 = b'\xD5'
+GRAPHIC_CUSTOM_7 = b'\xD6'
+GRAPHIC_CUSTOM_8 = b'\xD7'
 # Multiple interpretations of the graphics here.
 # Trains, teacups, beach and mouse is what I thought they were.
 # I've updated it so it has the "official names" of these graphics from the
 # manual as well, but not break the API.
-GRAPHIC_CITY = GRAPHIC_TRAINS = '\xD8'
-GRAPHIC_CARS = '\xD9'
-GRAPHIC_VESSEL = GRAPHIC_TEACUPS = '\xDA'
-GRAPHIC_TELEPHONE = '\xDB'
-GRAPHIC_NATURE = GRAPHIC_BEACH = '\xDC'
-GRAPHIC_SHIP = '\xDD'
-GRAPHIC_SWIM = '\xDE'
-GRAPHIC_CAT = GRAPHIC_MOUSE = '\xDF'
+GRAPHIC_CITY = GRAPHIC_TRAINS = b'\xD8'
+GRAPHIC_CARS = b'\xD9'
+GRAPHIC_VESSEL = GRAPHIC_TEACUPS = b'\xDA'
+GRAPHIC_TELEPHONE = b'\xDB'
+GRAPHIC_NATURE = GRAPHIC_BEACH = b'\xDC'
+GRAPHIC_SHIP = b'\xDD'
+GRAPHIC_SWIM = b'\xDE'
+GRAPHIC_CAT = GRAPHIC_MOUSE = b'\xDF'
 
-SOUND_BEEP_3 = '\xE0'
-SOUND_BEEP_5 = '\xE1'
-SOUND_BEEP_1 = '\xE2'
+SOUND_BEEP_3 = b'\xE0'
+SOUND_BEEP_5 = b'\xE1'
+SOUND_BEEP_1 = b'\xE2'
 
 
 class LEDSign:
@@ -184,11 +184,12 @@ class LEDSign:
         # print("Opening Port %s" % self.s.portstr)
 
     def send_to_sign(self, msg):
-        # byte_string = " ".join("{:02x}".format(ord(c)) for c in msg)
-        # print('Send: "{}" = "{}"'.format(msg, byte_string))
+        byte_string = " ".join("{:02x}".format(c) for c in msg)
+        print('Send: {} = "{}"'.format(msg, byte_string))
+        # print(bytes(msg)[0])
         self.s.write(msg)
 
-    def begin_message(self, sign=range(0, 128), reset=False):
+    def begin_message(self, sign=list(range(0, 128)), reset=False):
         """Begins a message for a sign.  defaults to all signs."""
         if self.message_open:
             raise Exception("A message is already open")
@@ -199,7 +200,7 @@ class LEDSign:
         except TypeError:
             pass
 
-        if type(sign) == int or type(sign) == long:
+        if type(sign) == int:
             # we're only programming a single sign.
             # we can optimise!
             if sign < 0 or sign > 127:
@@ -208,12 +209,12 @@ class LEDSign:
             # TODO: check whether this should be little or big endian properly.
             # the manual isn't really clear on this.  so lets take a stab in the dark.
 
-            self.send_to_sign("\x00" + chr(sign) + chr(sign))
+            self.send_to_sign(b"\x00" + bytes(sign) + bytes(sign))
 
             if reset:
-                self.send_to_sign("\x01")
+                self.send_to_sign(b"\x01")
             else:
-                self.send_to_sign("\x00")
+                self.send_to_sign(b"\x00")
 
         # we're done...
 
@@ -225,11 +226,11 @@ class LEDSign:
 
             # this is a special thing, doesn't have the \xFF terminator
             # start programming the sign, do not reset memory
-            self.send_to_sign("\x00\xFF\xFF")
+            self.send_to_sign(b"\x00\xFF\xFF")
             if reset:
-                self.send_to_sign("\x01")
+                self.send_to_sign(b"\x01")
             else:
-                self.send_to_sign("\x00")
+                self.send_to_sign(b"\x00")
 
             # # data is for sign list
             # self.send_to_sign("\x0B")
@@ -249,27 +250,29 @@ class LEDSign:
         if mode < 0 or mode > 24:
             raise Exception("run mode must be 0-24")
 
-        self.send_to_sign(chr(mode))
+        self.send_to_sign(bytes(mode))
 
     def add_text(self, msg):
-        msg = str(msg)
+        """Add text given as unicode string (sign can only render ascii characters)"""
+        # The sign can take ascii text
+        msg = msg.encode('ascii')
         for x in msg:
-            y = ord(x)
+            y = x
             if y < 32 or y > 237:
-                raise Exception("You shouldn't be using the character %s (#%s)." % (x, ord(x)))
+                raise Exception("You shouldn't be using the character %s (#%x)." % (x, x))
         self.send_to_sign(msg)
 
     def add_special(self, special):
-        self.send_to_sign("\xEF" + special)
+        self.send_to_sign(b"\xEF" + bytes(special))
 
     def end_frame(self):
-        self.send_to_sign('\xff')
+        self.send_to_sign(b'\xff')
 
     def end_message(self):
         if not self.message_open:
             raise Exception("A message isn't open")
         # this is a special thing, doesn't have the \xFF terminator
-        self.send_to_sign("\x00")
+        self.send_to_sign(b"\x00")
         self.message_open = False
 
     def set_clock(self, n=None, hour24=True):
@@ -278,11 +281,11 @@ class LEDSign:
         if n is None:
             n = datetime.now()
         if hour24:
-            hour24 = "\x00"
+            hour24 = b"\x00"
         else:
-            hour24 = "\x01"
+            hour24 = b"\x01"
         args = (pack("!B", n.isoweekday() % 7), hour24, n.year % 100, n.month, n.day, n.hour, n.minute, n.second)
-        self.send_to_sign(("\x08%s%s" + ("%02d" * 6) + '\xFF') % args)
+        self.send_to_sign((b"\x08%s%s" + (b"%02d" * 6) + b'\xFF') % args)
 
     def begin_file(self, file_id):
         if self.file_id is not None:
@@ -291,13 +294,13 @@ class LEDSign:
         if file_id < 0 or file_id > 99:
             raise Exception("file_id must be between 0 and 99.")
 
-        self.send_to_sign("\x01%02d" % file_id)
+        self.send_to_sign(b"\x01%02d" % file_id)
         self.file_id = file_id
 
     def end_file(self):
         if self.file_id is None:
             raise Exception("No file is open")
-        self.send_to_sign("\xFF\xFF")
+        self.send_to_sign(b"\xFF\xFF")
         self.file_id = None
 
     def display_page(self, pageid):
