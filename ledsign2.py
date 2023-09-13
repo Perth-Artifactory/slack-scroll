2bd33c624f1af390e07cdbb6a39b2dddc809b28c
tazard <16077190+tazard@users.noreply.github.com>
2023-09-13 10:40:50 +0800

    Tip for pyserial

--- ledsign2.py
+++ ledsign2.py
@@ -21,7 +21,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-import serial
+import serial  # pip install pyserial
 from datetime import datetime
 from struct import pack
 
