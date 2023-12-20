4cf763df75d7ffe32c3a71eb0a1cbde7d4cc6234
Blake Samuels <16077190+tazard@users.noreply.github.com>
2023-12-20 15:00:45 +0800

    Add start of web interface

---
+++ web_interface/src/web_interface.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import docker
+
+app = Flask(__name__)
+client = docker.from_env()
+
+@app.route('/reset')
+def reset_service():
+    try:
+        # Use Docker API to restart the service
+        client.containers.get('slackscroll_container_name').restart()
+        return 'Service restarted successfully', 200
+    except Exception as e:
+        return f'Failed to restart the service: {str(e)}', 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
